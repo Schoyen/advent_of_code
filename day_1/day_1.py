432025cd64112c5eeb9e58afc5514160c7b94d94
Schoyen <oyvindschoyen@gmail.com>
2016-12-12 20:38:30 +0100

    Add solution to day_1 part 2

--- day_1/day_1.py
+++ day_1/day_1.py
@@ -11,14 +11,20 @@ class EasterBunnyMap:
         rotation = 1 if direction == 'R' else -1
         self.current_direction = self.allowed_directions_keys[(self.allowed_directions_keys.index(self.current_direction) + rotation) % 4]
         movement = self.allowed_directions[self.current_direction]
-        self.current_position.append([movement[0]*steps + self.current_position[-1][0], movement[1]*steps + self.current_position[-1][1]])
+        for step in range(steps):
+            self.current_position.append([movement[0] + self.current_position[-1][0], movement[1] + self.current_position[-1][1]])
 
     def walk_the_walk(self):
-        from pylab import plot, show
         for direction in self.directions:
             self.do_step(direction[0], int(direction[1:]))
-        plot(*zip(*self.current_position))
-        show()
+
+    def find_crossing(self):
+        crossing = []
+        for position in self.current_position:
+            crossing.append(position)
+            if position in crossing[:-1]:
+                break
+        return crossing[-1]
 
 if __name__ == '__main__':
     with open('day1_input.dat', 'r') as f:
@@ -27,3 +33,5 @@ if __name__ == '__main__':
     ebm = EasterBunnyMap(directions)
     ebm.walk_the_walk()
     print ("Steps from Easter Bunny Headquarters: %d" % (abs(ebm.current_position[-1][0]) + abs(ebm.current_position[-1][1])))
+    crossing = ebm.find_crossing()
+    print ("Steps from first crossing: %d" % (abs(crossing[0]) + abs(crossing[1])))
