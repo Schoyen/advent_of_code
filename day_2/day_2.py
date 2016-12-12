d93b71b67ceda47c3313513b38ebd3b683343498
Schoyen <oyvindschoyen@gmail.com>
2016-12-12 21:32:01 +0100

    Start on __call__ method for Keypad

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -10,13 +10,26 @@ class Keypad:
     and methods for moving to a different button and storing the code sequence.
     """
 
-    def __init__(self, start_position='5'):
+    def __init__(self, start_position=5):
         self.position = start_position
         self.code = []
+        self.direction_value = {'U': 3, 'R': 1, 'L': -1, 'D': -3}
+        self.keypad = [1, 2, 3,
+                       4, 5, 6,
+                       7, 8, 9]
 
     def __call__(self, direction, press=False):
-        pass
+        allowed_directions = self.find_allowed_directions(self.position)
+        position = self.position + self.direction_value[direction]
+        if position < 1 or position > 9:
+            position = self.position
+
+    def find_allowed_directions(self, current_position):
+        directions = {}
+        index = self.keypad.index(current_position)
 
 if __name__ == '__main__':
     with open('day2_input.dat', 'r') as f:
         instruction_sequence = f.read()
+    key = Keypad()
+    key.find_allowed_directions(8)
