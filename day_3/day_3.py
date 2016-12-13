c5fd923cae272c3666dfd33eaea6ea22ee97dd07
Mint-kontor <oyvindschoyen@gmail.com>
2016-12-13 12:37:25 +0100

    Add solution to day 3 part 1

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,5 +1,24 @@
 
+class Triangle:
+
+    def __init__(self, sides):
+        self.sides = sides
+
+    def __call__(self):
+        return self.valid_triangle(self.sides)
+
+    def valid_triangle(self, sides):
+        for i in range(3):
+            side = sides.pop()
+            if side >= sum(sides):
+                return False
+            sides.insert(0, side)
+        return True
 
 if __name__ == '__main__':
     with open('day3_input.dat', 'r') as f:
-        triangles = [[int(element) for element in line.split()] for line in f]
+        triangles = [Triangle(map(int, line.split())) for line in f]
+    counter = 0
+    for triangle in triangles:
+        counter += triangle()
+    print ("The number of valid triangles is: %d" % (counter))
