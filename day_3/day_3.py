a0c2486385a84b20b7e367945d9a755c3e25ea6a
Mint-kontor <oyvindschoyen@gmail.com>
2016-12-13 12:53:27 +0100

    Add solution to day 3 part 2

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -15,10 +15,28 @@ class Triangle:
             sides.insert(0, side)
         return True
 
-if __name__ == '__main__':
-    with open('day3_input.dat', 'r') as f:
-        triangles = [Triangle(map(int, line.split())) for line in f]
+def count_triangles(triangles):
     counter = 0
     for triangle in triangles:
         counter += triangle()
-    print ("The number of valid triangles is: %d" % (counter))
+    return counter
+
+if __name__ == '__main__':
+    with open('day3_input.dat', 'r') as f:
+        triangles = [Triangle(map(int, line.split())) for line in f]
+
+    print ("The number of valid triangles is: %d" % (count_triangles(triangles)))
+
+    triangles = []
+    with open('day3_input.dat', 'r') as f:
+        tmp = [[], [], []]
+        for line in f:
+            side_1, side_2, side_3 = map(int, line.split())
+            tmp[0].append(side_1)
+            tmp[1].append(side_2)
+            tmp[2].append(side_3)
+            if len(tmp[0]) % 3 == 0:
+                triangles.extend(map(Triangle, tmp))
+                tmp = [[], [], []]
+
+    print ("The number of valid triangles is: %d" % (count_triangles(triangles)))
