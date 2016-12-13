d43c5cf7a3b403ffd4b94de6c28a72ca06b33c7e
Mint-kontor <oyvindschoyen@gmail.com>
2016-12-13 11:18:26 +0100

    Add solution to day 2 part 1

--- day_2/day_2.py
+++ day_2/day_2.py
@@ -13,23 +13,33 @@ class Keypad:
     def __init__(self, start_position=5):
         self.position = start_position
         self.code = []
-        self.direction_value = {'U': 3, 'R': 1, 'L': -1, 'D': -3}
-        self.keypad = [1, 2, 3,
-                       4, 5, 6,
-                       7, 8, 9]
 
     def __call__(self, direction, press=False):
         allowed_directions = self.find_allowed_directions(self.position)
-        position = self.position + self.direction_value[direction]
-        if position < 1 or position > 9:
-            position = self.position
+        self.position = allowed_directions[direction]
+        if press:
+            self.code.append(str(self.position))
+        return self.position
 
     def find_allowed_directions(self, current_position):
-        directions = {}
-        index = self.keypad.index(current_position)
+        row = (current_position - 1) // 3
+        column = (current_position - 1) % 3
+        directions = {'U': current_position - 3 if row > 0 else current_position,
+                      'R': current_position + 1 if column < 2 else current_position,
+                      'L': current_position - 1 if column > 0 else current_position,
+                      'D': current_position + 3 if row < 2 else current_position}
+        return directions
+
+def find_code(instructions):
+    key = Keypad()
+    for instruction in instructions:
+        for command in instruction[:-1]:
+            key(command)
+        key(instruction[-1], press=True)
+    return key.code
+
 
 if __name__ == '__main__':
     with open('day2_input.dat', 'r') as f:
-        instruction_sequence = f.read()
-    key = Keypad()
-    key.find_allowed_directions(8)
+        instruction_sequence = f.read().split()
+    print ("The code to the bathroom is: %s" % (''.join(find_code(instruction_sequence))))
