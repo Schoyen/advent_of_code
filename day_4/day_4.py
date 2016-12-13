5d6cffe5afcce432196c9e7b499c88c46e07d969
Mint-kontor <oyvindschoyen@gmail.com>
2016-12-13 14:53:29 +0100

    Add solution to day 4 part 2

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,7 +1,8 @@
 def is_valid_room(name, checksum):
     occurences = [0]*26
     for letter in name:
-        occurences[ord(letter) - 97] += 1
+        if ord(letter) != 45:
+            occurences[ord(letter) - 97] += 1
 
     for check in checksum:
         index = occurences.index(max(occurences))
@@ -11,13 +12,22 @@ def is_valid_room(name, checksum):
             return False
     return True
 
+def decrypt_name(name, sector):
+    decrypted_name = ''
+    rotation = sector % 26
+    for letter in name:
+        if letter == '-':
+            decrypted_name += ' '
+        else:
+            decrypted_name += chr(((ord(letter) + rotation) % 97) % 26 + 97)
+    return decrypted_name
+
 
 def separate_encryption(encrypted_string):
     name, number, checksum = '', '', ''
 
     for letter in encrypted_string[:-11]:
-        if ord(letter) != 45:
-            name += letter
+        name += letter
     for num in encrypted_string[-10:-7]:
         number += num
     for check in encrypted_string[-6:-1]:
@@ -30,9 +40,12 @@ if __name__ == '__main__':
         encrypted_rooms = [line.rstrip('\n')for line in f]
 
     sector_sum = 0
+    decrypted_names = []
     for room in encrypted_rooms:
         name, number, checksum = separate_encryption(room)
         if is_valid_room(name, checksum):
             sector_sum += number
+        decrypted_names.append(decrypt_name(name, number))
+        print ("%s (%d)" % (decrypted_names[-1], number))
 
     print ("The sum of the sectors for real rooms is: %d" % sector_sum)
