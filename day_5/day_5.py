84f7b62857767733e56282063dcaf8f864848261
Schoyen <oyvindschoyen@gmail.com>
2016-12-14 23:00:37 +0100

    Add solution to day 5 part 2

--- day_5/day_5.py
+++ day_5/day_5.py
@@ -7,14 +7,21 @@ def is_valid(door_ID, integer_index):
 def find_five_leading_zeros(door_ID, integer_index=0):
     while not is_valid(door_ID, integer_index):
         integer_index += 1
-    return md5("%s%d" % (door_ID, integer_index)).hexdigest()[5], (integer_index + 1)
+    return md5("%s%d" % (door_ID, integer_index)).hexdigest(), (integer_index + 1)
 
 if __name__ == '__main__':
     door_ID = "wtnhxymk"
     #door_ID = "abc"
-    password = ""
+    #password = ""
+    password_list = ['_']*8
     index = 0
-    while len(password) < 8:
+    while '_' in password_list:
         key, index = find_five_leading_zeros(door_ID, index)
-        password += str(key)
-    print ("The password to the door with ID %s is: %s" % (door_ID, password))
+        if 0 <= int(key[5], base=16) <= 7 and '_' == password_list[int(key[5], base=16)]:
+            print ("Found key %s at index %d" % (key, index - 1))
+            password_list[int(key[5], base=16)] = str(key[6])
+    print ("The password to the door with ID %s is: %s" % (door_ID, ''.join(password_list)))
+#    while len(password) < 8:
+#        key, index = find_five_leading_zeros(door_ID, index)
+#        password += str(key[5])
+#    print ("The password to the door with ID %s is: %s" % (door_ID, password))
