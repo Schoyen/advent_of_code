467cffb96aabf534eda519817c55f34eed79449e
Mint-kontor <oyvindschoyen@gmail.com>
2016-12-16 15:04:05 +0100

    Add documented read method in day 6

--- day_6/day6.py
+++ day_6/day6.py
@@ -1,5 +1,14 @@
 
 def read_data(filename):
+    """Read all data from filename and use split to create a list.
+
+    Input:
+        filename:                   The filename of the input data file.
+
+    Returns:
+        list:                       A list with all the data. The elements are
+                                    separated strings in the original file.
+    """
     with open(filename, 'r') as f:
         return f.read().split()
 
