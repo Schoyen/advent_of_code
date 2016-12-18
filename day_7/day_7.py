7c4b094e7225a1ff9624dfbbb718764e5a0f9e75
Schoyen <oyvindschoyen@gmail.com>
2016-12-18 16:51:12 +0100

    Add method detecting 4-char symmetric sequences.

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -1,4 +1,16 @@
 
+def determine_ABBA_(ipv7_address):
+    pass
+
+def _determine_symmetric_string(string):
+    if len(string) < 4:
+        return False
+
+    for i in range(len(string) - 3):
+        if string[i] != string[i+1] and string[i] == string[i+3] and string[i+1] == string[i+2]:
+            return True
+
+    return False
 
 if __name__ == '__main__':
     with open('day7_input.dat', 'r') as f:
