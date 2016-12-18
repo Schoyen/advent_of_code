989429a40b4522ac7b271c72a676639f1b366b8d
Schoyen <oyvindschoyen@gmail.com>
2016-12-18 17:18:58 +0100

    Add test function for TLS-detection

--- day_7/day_7.py
+++ day_7/day_7.py
@@ -23,6 +23,24 @@ def _determine_symmetric_string(string):
 
     return False
 
+def test_example_values():
+    example_data = [
+            "abba[mnop]qrst",
+            "abcd[bddb]xyyx",
+            "aaaa[qwer]tyui",
+            "ioxxoj[asdfgh]zxcvbn"
+            ]
+    expected_result = [
+            True,
+            False,
+            False,
+            True
+            ]
+
+    for expected, data in zip(expected_result, example_data):
+        assert expected == _determine_ABBA(data)
+
 if __name__ == '__main__':
     with open('day7_input.dat', 'r') as f:
         data = f.read().split()
+
